38fd18cc2ec59884629532b568a3e258c36bf88a
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-19 11:09:23 +0900

    RSpec,/signupに正常にアクセスできること

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
+  describe "GET /signup" do
+    it "正常にアクセスできること" do
+      get "/signup"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
